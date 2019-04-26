6a6ee37a96f56310b9a9417c1ddff5990bfed8bc
Pierantonio Petralia <41144486+stealth90@users.noreply.github.com>
2019-04-26 22:31:39 +0200

    Update Tris.java

--- src/com/tris/Tris.java
+++ src/com/tris/Tris.java
@@ -10,7 +10,7 @@ public class Tris {
         this.board = new String[3][3];
         for (int i=0;i<3;i++){
             for(int j=0; j<3;j++){
-                board[i][j]="";
+                board[i][j]=" ";
             }
         }
         }
