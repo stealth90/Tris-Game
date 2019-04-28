c11ad51a723989341308d0afb8d1106536a65f8b
Pierantonio Petralia <41144486+stealth90@users.noreply.github.com>
2019-04-28 20:18:41 +0200

    Update Main.java
    Add try/catch functionality.

--- src/com/tris/Main.java
+++ src/com/tris/Main.java
@@ -1,5 +1,6 @@
 package com.tris;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,11 +11,8 @@ public class Main {
         boolean exit = false;
         System.out.println("Welcome in my Tris game");
         printMenu();
-        try {
         while (!exit) {
-            System.out.println("Do your choice");
-            choice = scanner.nextInt();
-            scanner.nextLine();
+            choice=getIntForMenu();
             switch (choice) {
                 case 0:
                     Tris a = new Tris();
@@ -23,19 +21,22 @@ public class Main {
                     String gameTurn;
                     while (!gameOver) {
                         printGameMenu();
-                        System.out.println("Do your choice");
-                        choiceMove = scanner.nextInt();
+                        choiceMove = getIntForMenu();
                         switch (choiceMove) {
                             case 0:
                                 System.out.println("Which player start the game? X or O");
-                                scanner.nextLine();
                                 String playerGame = scanner.nextLine();
                                 System.out.println("Which position of row u want sign ?");
-                                int xCoordinate = scanner.nextInt();
+                                int xCoordinate = getCoordinate();
                                 System.out.println("Which position of column u want sign?");
-                                int yCoordinate = scanner.nextInt();
-                                gameTurn = a.setSign(xCoordinate, yCoordinate, playerGame);
-                                System.out.println(gameTurn);
+                                int yCoordinate = getCoordinate();
+                                try{
+                                    gameTurn = a.setSign(xCoordinate, yCoordinate, playerGame);
+                                    System.out.println(gameTurn);
+                                }catch (ArrayIndexOutOfBoundsException invalidIndexArray){
+                                    System.out.println("Insert a valid coordinate");
+                                    System.out.println("Turn invalid");
+                                }
                                 break;
 
                             case 1:
@@ -58,12 +59,7 @@ public class Main {
                     System.out.println("Please, insert right choice");
                     break;
             }
-
         }
-        } catch(java.util.InputMismatchException excNumber){
-        System.out.println("Insert a number of that printed");
-    }
-
     }
     public static void printMenu(){
         System.out.println("What do you want?");
@@ -75,6 +71,31 @@ public class Main {
         System.out.println("What do you want ?");
         System.out.println("\t 0 - Insert your move");
         System.out.println("\t 1 - Return to main menu");
+    }
+
+    public static int getCoordinate(){
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            try{
+                return scanner.nextInt();
+            }catch(InputMismatchException inputNotANumber){
+                scanner.nextLine();
+                System.out.println("Please insert a valid number");
+            }
+        }
+    }
+
+    public static int getIntForMenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do your choice");
+        while(true){
+            try{
+                return scanner.nextInt();
+            }catch (InputMismatchException inputNotANumber){
+                scanner.nextLine();
+                System.out.println("Please insert a number");
+            }
+        }
     }
 
 }
